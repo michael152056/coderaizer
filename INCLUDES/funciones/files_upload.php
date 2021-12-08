ca64f9824b398be884a3b89778bbcd90dc892316
michael152056 <75760078+michael152056@users.noreply.github.com>
2021-12-08 11:44:24 -0500

    Update files_upload.php

--- INCLUDES/funciones/files_upload.php
+++ INCLUDES/funciones/files_upload.php
@@ -9,20 +9,11 @@ if($flag == "code"){
     $tmp_name = $_FILES[$fichero]['tmp_name'];
     $img_explode = explode('.',$img_name);
     $img_ext = end($img_explode);
-    $extensions = ["py"];
-    if(in_array($img_ext, $extensions) === true){
-        $types = ["text/x-python"];
-        if(in_array($img_type, $types) === true){
+
             $time = time();
             $new_img_name = $time.$img_name;
             if(move_uploaded_file($tmp_name,"../../code/".$new_img_name)){
                echo json_encode($new_img_name);
             }
-        }else{
-            echo "Porfavor sube un archivo .py";
-        }
-    }else{
-        echo "Porfavor sube un archivo .py";
-    }
 }
 ?>
