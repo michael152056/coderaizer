bd955ff56d8ec3a3719c5e2df7bb02a586c7434a
michael152056 <75760078+michael152056@users.noreply.github.com>
2021-12-13 22:17:42 -0500

    Update mapa.php

--- INCLUDES/funciones/mapa.php
+++ INCLUDES/funciones/mapa.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: image/png");
-$stuff = system('python ../../code/heatmap.py', $output);
+$stuff = exec('python ../../code/heatmap.py', $output);
 foreach($output as $key=>$value){
     if($key==1)
         print chr(0x0D); //Newline feed after PNG declaration
